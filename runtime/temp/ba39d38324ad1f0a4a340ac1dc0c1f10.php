5111998102a1cbda3ca6f109bf472e7169787357
root <admin@example.com>
2022-06-19 22:35:38 +0800

    init

--- runtime/temp/ba39d38324ad1f0a4a340ac1dc0c1f10.php
+++
@@ -1,62 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:4:{s:72:"/www/wwwroot/dst/public/../application/admin/view/stock/add_piliang.html";i:1655121070;s:59:"/www/wwwroot/dst/application/admin/view/layout/default.html";i:1655121070;s:56:"/www/wwwroot/dst/application/admin/view/common/meta.html";i:1655121070;s:58:"/www/wwwroot/dst/application/admin/view/common/script.html";i:1655121070;}*/ ?>
-<!DOCTYPE html>
-<html lang="<?php echo $config['language']; ?>">
-    <head>
-        <meta charset="utf-8">
-<title><?php echo (isset($title) && ($title !== '')?$title:''); ?></title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-<meta name="renderer" content="webkit">
-
-<link rel="shortcut icon" href="/assets/img/favicon.ico" />
-<!-- Loading Bootstrap -->
-<link href="/assets/css/backend.css?v=<?php echo \think\Config::get('site.version'); ?>" rel="stylesheet">
-
-<!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
-<!--[if lt IE 9]>
-  <script src="/assets/js/html5shiv.js"></script>
-  <script src="/assets/js/respond.min.js"></script>
-<![endif]-->
-<script type="text/javascript">
-    var require = {
-        config:  <?php echo json_encode($config); ?>
-    };
-</script>
-    </head>
-
-    <body class="inside-header inside-aside <?php echo defined('IS_DIALOG') && IS_DIALOG ? 'is-dialog' : ''; ?>">
-        <div id="main" role="main">
-            <div class="tab-content tab-addtabs">
-                <div id="content">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            <section class="content-header hide">
-                                <h1>
-                                    <?php echo __('Dashboard'); ?>
-                                    <small><?php echo __('Control panel'); ?></small>
-                                </h1>
-                            </section>
-                            <?php if(!IS_DIALOG && !\think\Config::get('fastadmin.multiplenav')): ?>
-                            <!-- RIBBON -->
-                            <div id="ribbon">
-                               <!-- <ol class="breadcrumb pull-left">
-                                    <li><a href="dashboard" class="addtabsit"><i class="fa fa-dashboard"></i> <?php echo __('Dashboard'); ?></a></li>
-                                </ol>
-                                <ol class="breadcrumb pull-right">
-                                    <?php foreach($breadcrumb as $vo): ?>
-                                    <li><a href="javascript:;" data-url="<?php echo $vo['url']; ?>"><?php echo $vo['title']; ?></a></li>
-                                    <?php endforeach; ?>
-                                </ol>-->
-                            </div>
-                            <!-- END RIBBON -->
-                            <?php endif; ?>
-                            <div class="content">
-                                    <div class="content-main">    <div class="layui-fluid">        <div class="layui-card">            <div class="layui-card-header">                        </div>            <div class="layui-card-body">                <form class="layui-form">                    <div class="layui-form-item">                        <label class="layui-form-label">视频信息</label>                        <div class="layui-input-block">                        	<textarea id="video_msg" name="video_msg" placeholder="视频信息格式：【111】视频标题|视频地址|图片地址  " class="layui-input" style="height:300px"></textarea>                            <span class="text-warning">中括号中是分类,系统系统匹配,一行一个切勿串行,分类如果没有可空.</span>                        </div>                    </div>                    <div class="layui-form-item">                        <label class="layui-form-label">类型</label>                        <div class="layui-input-block">                            <?php echo build_select('row[cid]', $cat, '', ['class'=>'form-control', 'id' => 'selects','required'=>'required']); ?>                        </div>                    </div>                                                             <div class="layui-form-item">                        <label class="layui-form-label">导入顺序</label>                        <div class="layui-input-block">                            <select class="form-control" id="sort" required="required" name="row[sort]">                                <option value='0' >--默认顺序--</option>                                                                                                <option value='1'>标题|视频|图片</option>                                <option value='2'>标题|图片|视频</option>                                <option value='3'>视频|图片|标题</option>                                <option value='4'>视频|标题|图片</option>                                <option value='5'>图片|视频|标题</option>                                <option value='6'>图片|标题|视频</option>                                </select>                        </div>                    </div>                                                           <div class="layui-form-item">                        <button type="button" class="layui-btn">提交</button>                    </div>                </form>                </div>            </div>        </div>    </div>    <script type="text/javascript" src="/assets/libs/layui/layui.all.js"></script><script>       layui.use(['jquery','form'], function(){        var form = layui.form            ,$=layui.jquery;        $(document).ready(function(){            $(".layui-btn").click(function(){                                console.log($("#sort"))                $.ajax({                    url: "",                    type : "POST",                    dataType:'json',                    data:{                        video_msg:$("#video_msg").val(),                        cid:$("#selects").find("option:selected").val(),                                                sort:$("#sort").find("option:selected").val(),                    },                    cache: false,                    async:false,                    success: function (data) {                        if (data.code == 0) {                            layer.msg(data.msg,{time:1500},function(){                                parent.layer.closeAll();                                window.parent.location.reload();                            });                        }else{                            layer.msg(data.msg);                        }                    }                });            });            return false;        });    });</script>    </block>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <script src="/assets/js/require.js" data-main="/assets/js/require-backend.js?v=<?php echo htmlentities($site['version']); ?>"></script>
-    </body>
-</html>
